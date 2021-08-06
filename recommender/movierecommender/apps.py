c1c2f9c5e971c2defe0a0d04ba3afb2cf600bbf2
Yan Luo <yluo@ca.ibm.com>
2021-08-06 12:12:46 -0400

    added skeleton app

--- recommender/movierecommender/apps.py
+++ recommender/movierecommender/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MovieRecommenderConfig(AppConfig):
+class MovierecommenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movie_recommender'
+    name = 'movierecommender'
