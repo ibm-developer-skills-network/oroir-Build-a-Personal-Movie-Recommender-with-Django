ab5a6daa419c95aed2ee5e0cfa57bee66d680939
yluo@ca.ibm.com <yluo@ca.ibm.com>
2021-08-12 16:40:36 -0400

    Update load_movies.py

--- app_template/recommender/movierecommender/management/commands/load_movies.py
+++ app_template/recommender/movierecommender/management/commands/load_movies.py
@@ -10,10 +10,13 @@ class Command(BaseCommand):
         parser.add_argument('--path', type=str)
 
     def handle(self, *args, **kwargs):
+        # Remove any existing data
         print("Clean old movie data")
         Movie.objects.all().delete()
         path = kwargs['path']
+        # Read the movie csv file as a dataframe
         movie_df = pd.read_csv(path)
+        # Iterate each row in the dataframe
         for index, row in movie_df.iterrows():
             imdb_id = row["imdb_id"]
             genres = row["genres"]
@@ -24,6 +27,7 @@ class Command(BaseCommand):
             vote_average = row["vote_average"]
             vote_count = row["vote_count"]
             poster_path = row["poster_path"]
+            # Populate Movie object for each row
             movie = Movie(imdb_id=imdb_id,
                             genres=genres,
                             original_title=original_title,
@@ -33,6 +37,7 @@ class Command(BaseCommand):
                             vote_average=vote_average,
                             vote_count=vote_count,
                             poster_path=poster_path)
+            # Save movie object
             movie.save()
             print(f"Movie: {imdb_id}, {original_title} saved...")
 
